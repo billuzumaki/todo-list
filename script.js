d4ccaf2325d7ea9c64d11675e0182231b23c3f75
billuzumaki <santos-emiliano@hotmail.com>
2023-10-28 08:10:21 -0300

    firt commit

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-function calcularMontante() {
-    let valorInicial = parseFloat(document.getElementById("valor-inicial").value);
-    let taxaJuros = parseFloat(document.getElementById("taxa-juros").value);
-    let tempo = parseFloat(document.getElementById("tempo").value);
-    let depositoMensal = parseFloat(document.getElementById("deposito-mensal").value);
-  
-    let montante = calcularJurosCompostos(valorInicial, taxaJuros, tempo, depositoMensal);
-    document.getElementById("montante-final").textContent = montante;
-  }
-  
-  function calcularJurosCompostos(valorInicial, taxaJuros, tempo, depositoMensal) {
-    let montante = valorInicial;
-    for (let i = 1; i <= tempo * 12; i++) {
-      montante += depositoMensal;
-      montante *= 1 + taxaJuros/100/12;
+const totalBox = [] 
+const list = document.getElementById('Todolist');
+const result = document.getElementById("result");
+const today = new Date();
+const dayOfWeek = today.getDay();
+
+// Convert the day of the week to a string
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const dayName = daysOfWeek[dayOfWeek];
+
+
+function add() {
+    const boxes = document.querySelector('.box').value;
+    const li = document.createElement('li');
+
+    if(boxes !== "" && ! isNaN(boxes) && list.childNodes.length < 11) {
+        li.innerHTML = `${dayName}  ${boxes}`;
+        list.appendChild(li);
+
+        totalBox.push(parseInt(boxes)); 
     }
-    return montante.toFixed(2);
-  }
-  
+    
+}
+
+function sum() {
+    if(totalBox.length != 0) {
+        const sum = totalBox.reduce((total, valor) => total + valor);
+        result.innerText = `Total:  ${sum}`;
+    } else {
+        result.innerText = 'Total: 0';
+    }
+}
+
+function remove() {
+    if(list.childNodes.length > 0) {
+
+        const lastItem = list.lastChild;
+        totalBox.pop();
+        list.removeChild(lastItem);
+    }
+}
